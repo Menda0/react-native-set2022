2f7bfff06590a936724a9f8bc2117bc68d16c17a
Marco Mendão <mac.mendao@gmail.com>
2022-10-10 20:25:28 +0100

    Add new stuf

--- javascript-intro/helloworld.js
+++ javascript-intro/helloworld.js
@@ -1,3 +1,5 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 console.log("Hello world")
 
 // Not recommend to use
@@ -120,3 +122,144 @@ const circleArea3 = (r) => {
 const circleArea4 = (r) => 3.14 * r * r
 // Can be used only if one parameter function 
 const cirlceArea5 = r => 3.14 * r * r
+
+// Simple promise
+const p1 = new Promise(() => {})
+
+console.log(p1)
+
+// fulfilled: The promise as been resolved
+// rejected: The promise as been rejected
+// pending:  Default state promise neither resolved or reject
+
+// param resolve and reject are functions
+const p2 = new Promise((resolve, reject) => {
+    resolve("This is a promise that went fine")
+})
+
+
+console.log("Start my code ...")
+// then is a method inside object Promise that receives a function () => {}
+// Callback method then and catch
+//p2.then((result) => {console.log(result)})
+// Never gona be executed because p2 always resolves
+// p2.catch((err) => {console.log(err)})
+
+// const p3 = new Promise((resolve, reject) => {
+//     reject("This is a promise that went bad")
+// })
+
+// Promise that went bad
+//p3.catch(err => console.log("Fatal error",err))
+
+console.log("End my code ...")
+
+function circleArea6(r){
+    return new Promise((resolve, reject) => {
+        if(r <= 0){
+            reject("Cannot calculate area with radius <= 0")
+        }else{
+            resolve(r*r*3.14)
+        }
+    })
+}
+
+// circleArea6(10).then((result) => {console.log(result)})
+
+// circleArea6(-10).catch(err => console.log(err))
+
+const url = "https://api.chucknorris.io/jokes/random"
+
+// const httpRequest = new XMLHttpRequest()
+// httpRequest.onreadystatechange = () => {
+//     if(httpRequest.readyState === 4 && httpRequest.status === 200){
+//         const joke = JSON.parse(httpRequest.responseText)
+//         console.log(joke.value)
+//     }
+// }
+
+// httpRequest.open("GET", url, true)
+// httpRequest.send()
+
+function getChuckNorrisJoke(){
+    return new Promise((resolve, reject) => {
+        const url = "https://api.chucknorris.io/jokes/random"
+        const httpRequest = new XMLHttpRequest()
+        httpRequest.onreadystatechange = function () {
+            if(httpRequest.readyState === 4 && httpRequest.status === 200){
+                const joke = JSON.parse(httpRequest.responseText)
+                resolve(joke)
+            }
+        }
+        httpRequest.open("GET", url, true)
+        httpRequest.send()
+    })
+}
+
+// Then, catch and finally (callback method)
+console.log("Start ...")
+getChuckNorrisJoke().then(joke => console.log("Joke 1",joke.value))
+getChuckNorrisJoke().then(joke => console.log("Joke 2",joke.value))
+getChuckNorrisJoke().then(joke => console.log("Joke 3",joke.value))
+console.log("End ...")
+
+// Async away
+
+// Cannot apply sincronious programing rules to async programing
+// This is wrong and dont work
+/*
+function getChuckNorrisJokeList(){
+    const jokes = []
+
+    getChuckNorrisJoke().then(joke => {
+        jokes.push(joke)
+    })
+    getChuckNorrisJoke().then(joke => {
+        jokes.push(joke)
+    })
+    getChuckNorrisJoke().then(joke => {
+        jokes.push(joke)
+    })
+    getChuckNorrisJoke().then(joke => {
+        jokes.push(joke)
+    })
+
+    return jokes
+}
+*/
+
+// Works but its not recomended
+// This is a callback hell
+// We are using multiple async function 
+// Please use async await method
+function getChuckNorrisJokeList(){
+
+    getChuckNorrisJoke().then(joke1 => {
+        getChuckNorrisJoke().then(joke2 => {
+            getChuckNorrisJoke().then( joke3 => {
+                console.log("jokes =", [joke1.value,joke2.value,joke3.value])
+            })
+        })
+    })
+}
+
+async function getChuckNorrisJokeList2() {
+    const joke1 = await getChuckNorrisJoke()
+    const joke2 = await getChuckNorrisJoke()
+    const joke3 = await getChuckNorrisJoke()
+    const joke4 = await getChuckNorrisJoke()
+
+    console.log("jokes2 =", [joke1.value, joke2.value, joke3.value, joke4.value])
+}
+
+async function getChuckNorrisJokeList3() {
+    const promises = [getChuckNorrisJoke(), getChuckNorrisJoke(), getChuckNorrisJoke()]
+
+    const result = await Promise.all(promises)
+
+    return result
+}
+
+getChuckNorrisJokeList()
+getChuckNorrisJokeList2()
+getChuckNorrisJokeList3()
